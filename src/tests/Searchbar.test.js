0020bf74636738073b1367dcf5595499cacd9754
unknown <albertkantwi@gmail.com>
2023-09-14 14:16:43 +0100

    Add test to components

---
+++ src/tests/Searchbar.test.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { render, fireEvent } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom';
+import Searchbox from '../components/Searchbar';
+import store from '../redux/store';
+
+test('Check if the Search component is rendered correctly', () => {
+    const tree = render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Searchbox />
+        </BrowserRouter>
+      </Provider>,
+    );
+  
+    expect(tree).toMatchSnapshot();
+  });
