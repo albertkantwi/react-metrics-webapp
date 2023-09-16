fe4d286c11d91ecd6cd33e3ad6bc11c8a5987795
unknown <albertkantwi@gmail.com>
2023-09-16 20:08:03 +0100

    Add units test to componenets

--- src/tests/Population.test.js
+++ src/tests/Population.test.js
@@ -2,17 +2,43 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import store from '../redux/store';
+import configureStore from 'redux-mock-store';
 import Population from '../components/Population';
 
-test('Check if the Population component is rendered correctly', () => {
-  const tree = render(
-    <Provider store={store}>
-      <BrowserRouter>
+const mockStore = configureStore();
+const store = mockStore({
+  country: {
+    countriesData: [
+      { name: 'Canada', population: 38005238 },
+      { name: 'Morocco', population: 36910558 },
+    ],
+  },
+});
+describe('Population Component', () => {
+  test('Check if the Population component is rendered correctly', () => {
+    const tree = render(
+      <Provider store={store}>
+        <BrowserRouter>
+          <Population />
+        </BrowserRouter>
+      </Provider>,
+    );
+    expect(tree).toMatchSnapshot();
+  });
+  test('renders without crashing', () => {
+    render(
+      <Provider store={store}>
         <Population />
-      </BrowserRouter>
-    </Provider>,
-  );
-
-  expect(tree).toMatchSnapshot();
+      </Provider>,
+    );
+  });
+  test('calculates and displays total population correctly', () => {
+    const { getByText } = render(
+      <Provider store={store}>
+        <Population />
+      </Provider>,
+    );
+    const totalPopulationText = getByText('74,915,796');
+    expect(totalPopulationText).toBeInTheDocument();
+  });
 });
